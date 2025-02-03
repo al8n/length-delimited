509260b00d28265a8139f587a708e30a9d8b08f1
al8n <scygliu1@gmail.com>
2025-02-04 01:28:58 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,53 +18,71 @@ extern crate std;
 pub use sealed::{Bounds, ErrorBounds};
 
 mod sealed {
-  #[cfg(not(any(feature = "send", feature = "sync", feature = "static")))]
-  pub trait Bounds {}
+  macro_rules! doc {
+    (@bounds { $($item:item)* }) => {
+      $(
+        /// A trait bound that bounds `Send`, `Sync`, and `'static` in possible combinations
+        ///
+        /// which can be configured with features.
+        $item
+      )*
+    };
+    (@error_bounds { $($item:item)* }) => {
+      $(
+        /// A trait bound that bounds `Debug`, `Display`, `Error`, `Send`, `Sync`, and `'static` in possible combinations
+        ///
+        /// which can be configured with features.
+        $item
+      )*
+    };
+  }
+
+  doc!(@bounds {
+    #[cfg(not(any(feature = "send", feature = "sync", feature = "static")))]
+    pub trait Bounds {}
+
+    #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
+    pub trait Bounds: Sync {}
+
+    #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
+    pub trait Bounds: Send {}
+
+    #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
+    pub trait Bounds: 'static {}
+
+    #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
+    pub trait Bounds: Send + Sync {}
+    
+    #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
+    pub trait Bounds: Sync + 'static {}
+
+    #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
+    pub trait Bounds: Send + 'static {}
+  
+    #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
+    pub trait Bounds: Send + Sync + 'static {}
+  });
 
   #[cfg(not(any(feature = "send", feature = "sync", feature = "static")))]
   impl<T> Bounds for T {}
 
   #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
-  pub trait Bounds: Sync {}
-
-  #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
   impl<T> Bounds for T where T: Sync {}
-
-  #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
-  pub trait Bounds: Send {}
 
   #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
   impl<T> Bounds for T where T: Send {}
 
   #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
-  pub trait Bounds: 'static {}
-
-  #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
   impl<T> Bounds for T where T: 'static {}
-
-  #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
-  pub trait Bounds: Send + Sync {}
 
   #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
   impl<T> Bounds for T where T: Send + Sync {}
 
   #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
-  pub trait Bounds: Sync + 'static {}
-
-  #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
   impl<T> Bounds for T where T: Sync + 'static {}
 
   #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
-  pub trait Bounds: Send + 'static {}
-
-  #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
   impl<T> Bounds for T where T: Send + 'static {}
-
-  /// A trait bound that bounds `Send`, `Sync`, and `'static` in possible combinations
-  ///
-  /// which can be configured with features.
-  #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
-  pub trait Bounds: Send + Sync + 'static {}
 
   #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
   impl<T> Bounds for T where T: Send + Sync + 'static {}
@@ -100,55 +118,54 @@ mod sealed {
   impl<T> Printable for T where T: core::error::Error {}
 
   #[cfg(not(any(feature = "send", feature = "sync", feature = "static",)))]
-  pub trait ErrorBounds: Printable {}
-
-  #[cfg(not(any(feature = "send", feature = "sync", feature = "static",)))]
   impl<T> ErrorBounds for T where T: Printable {}
-
-  #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
-  pub trait ErrorBounds: Printable + Sync {}
 
   #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
   impl<T> ErrorBounds for T where T: Printable + Sync {}
 
   #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
-  pub trait ErrorBounds: Printable + Send {}
-
-  #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
   impl<T> ErrorBounds for T where T: Printable + Send {}
-
-  #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
-  pub trait ErrorBounds: Printable + 'static {}
 
   #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
   impl<T> ErrorBounds for T where T: Printable + 'static {}
 
   #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
-  pub trait ErrorBounds: Printable + Send + Sync {}
-
-  #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
   impl<T> ErrorBounds for T where T: Printable + Send + Sync {}
-
-  #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
-  pub trait ErrorBounds: Printable + Sync + 'static {}
 
   #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
   impl<T> ErrorBounds for T where T: Printable + Sync + 'static {}
 
   #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
-  pub trait ErrorBounds: Printable + Send + 'static {}
-
-  #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
   impl<T> ErrorBounds for T where T: Printable + Send + 'static {}
-
-  /// A trait bound that bounds `Debug`, `Display`, `Error`, `Send`, `Sync`, and `'static` in possible combinations
-  ///
-  /// which can be configured with features.
-  #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
-  pub trait ErrorBounds: Printable + Send + Sync + 'static {}
 
   #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
   impl<T> ErrorBounds for T where T: Printable + Send + Sync + 'static {}
+
+  doc!(@error_bounds {
+    #[cfg(not(any(feature = "send", feature = "sync", feature = "static",)))]
+    pub trait ErrorBounds: Printable {}
+
+    #[cfg(all(feature = "sync", not(any(feature = "send", feature = "static"))))]
+    pub trait ErrorBounds: Printable + Sync {}
+
+    #[cfg(all(feature = "send", not(any(feature = "sync", feature = "static"))))]
+    pub trait ErrorBounds: Printable + Send {}
+
+    #[cfg(all(feature = "static", not(any(feature = "send", feature = "sync"))))]
+    pub trait ErrorBounds: Printable + 'static {}
+
+    #[cfg(all(feature = "sync", feature = "send", not(feature = "static")))]
+    pub trait ErrorBounds: Printable + Send + Sync {}
+
+    #[cfg(all(feature = "sync", feature = "static", not(feature = "send")))]
+    pub trait ErrorBounds: Printable + Sync + 'static {}
+
+    #[cfg(all(feature = "send", feature = "static", not(feature = "sync")))]
+    pub trait ErrorBounds: Printable + Send + 'static {}
+
+    #[cfg(all(feature = "send", feature = "sync", feature = "static"))]
+    pub trait ErrorBounds: Printable + Send + Sync + 'static {}
+  });
 }
 
 /// A type can be encode to a buffer.
